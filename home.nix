38bf27e9906b68cc5df3725a07ca82e216255402
Oliver Dennis <oliver.d.333@proton.me>
2025-01-30 16:10:48 +0000

    Theming kitty and waybar to tokyo night night

--- home.nix
+++ home.nix
@@ -134,10 +134,11 @@
       font_size = "12.0";
       window_padding_width = "10.0 10.0";
       margin_width = "0.0";
-      background_opacity = "0.5";
+      # background_opacity = "0.5";
       allow_remote_control = "yes";
       dynamic_background_opacity = "yes";
     };
+    themeFile = "tokyo_night_night";
   };
 
   programs.zoxide = {
